10f3256a910030a1751f2a818e55c451dbb7e26a
Estudiante <Estudiante@192.168.8.227>
2019-07-02 14:53:15 -0500

    Se inicia el modelo

---
+++ src/Modelo/Tarea.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Estudiante
+ */
+ class Tarea {
+    
+   private static boolean estado; 
+   private Date contador;
+   private List <String> descripcion;
+    
+    
+}
